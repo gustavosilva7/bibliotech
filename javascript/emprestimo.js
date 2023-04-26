37d9296f7e340d797c25603de964312de24aa50e
calma-end7 <gustavosilvaalexgusta007@gmail.com>
2023-04-26 20:57:23 -0300

    gustavo

--- javascript/emprestimo.js
+++ javascript/emprestimo.js
@@ -1,57 +1,109 @@
 const inputNomeLivro = document.getElementById("inputEmprestimoLivro")
+const divLivro = document.getElementById("divNomeLivro");
+const modalOptionsLivro = document.createElement("div");
 
-const inputNomeAluno = document.getElementById("inputEmprestimoNomeAluno")
+
+modalOptionsLivro.style.display = "flex";
+modalOptionsLivro.style.flexDirection = "column";
+modalOptionsLivro.style.height = "150px";
+modalOptionsLivro.style.width = "100%";
+modalOptionsLivro.style.position = "absolute";
+modalOptionsLivro.style.top = "100%";
+modalOptionsLivro.style.background = "#fff";
 
 
-inputNomeAluno.addEventListener("input", (event)=>{
-    fetch("../json/alunos.json").then(respAluno =>{
-        respAluno.json().then((dados) =>{
-            dados.alunosGerais.forEach((aluno) =>{
+inputNomeLivro.addEventListener("input", (event) => {
+    const inputname = event.target.value.trim();
+    fetch("../json/livros.json").then(respLivro => {
+        respLivro.json().then((dados) => {
+            modalOptionsLivro.innerHTML = "";
+            dados.livrosGerais.forEach((livro) => {
+                const nome = livro.livro;
+                const teste = nome.toLowerCase().includes(inputname.toLowerCase());
 
-                const inputname = event.target.value.trim()
-                // console.log({aluno})
-                const nome = aluno.nomeDoAluno
 
-                // const teste = nomes.indexOf(inputname) != -1 ? 'ok' : 'não ok'
-                
-                const teste = nome.includes(inputname);
-                if(teste){
-                    const inputSerie = aluno.serie
-                    const inputTurma = aluno.turma
-                    document.getElementById('inputTurma').value = inputTurma;
+                if (teste) {
+                    const opBook = document.createElement("a");
+                    
+                    opBook.style.width = "100%";
+                    opBook.style.fontSize = "60%"
+                    
+                    opBook.innerText = `${livro.livro} || ${livro.tombo} || ${livro.autor}`;
+                    
+                    modalOptionsLivro.appendChild(opBook);
+                    opBook.addEventListener("click", () => {
+                        inputNomeLivro.value = livro.livro;
+                        document.getElementById('inputEmprestimoTombo').value = livro.tombo;
+                        document.getElementById('inputEmprestimoAutor').value = livro.autor;
+                        modalOptionsLivro.style.display = "none";
+                    });
                 }
-                
-               
-                console.log(inputname)
-                console.log(teste)
-                
+            });
+            divLivro.appendChild(modalOptionsLivro);
+            modalOptionsLivro.style.display = "flex";
+        });
+    });
+});
+
+document.addEventListener("click", (event) => {
+    if (!event.target.matches("#inputEmprestimoNomeAluno") && !event.target.matches("option")) {
+        modalOptionsLivro.style.display = "none";
+    }
+});
 
 
-   
-                // nomes.forEach(nome =>{
-                //     console.log(nome.textContent == input )
-                // // })
-                // console.log(respAluno)
-                // console.log(dados)
-                // console.log(aluno)
-                // console.log({nomes})
-            })
-        })
-    })
-})
+const inputNomeAluno = document.getElementById("inputEmprestimoNomeAluno");
+const divNome = document.getElementById("divNomeDoAluno");
+const modalOptions = document.createElement("div");
 
 
+modalOptions.style.display = "flex";
+modalOptions.style.flexDirection = "column";
+modalOptions.style.height = "150px";
+modalOptions.style.width = "100%";
+modalOptions.style.position = "absolute";
+modalOptions.style.top = "100%";
+modalOptions.style.background = "#fff";
 
 
+inputNomeAluno.addEventListener("input", (event) => {
+    const inputname = event.target.value.trim();
+    fetch("../json/alunos.json").then(respAluno => {
+        respAluno.json().then((dados) => {
+            modalOptions.innerHTML = "";
+            dados.alunosGerais.forEach((aluno) => {
+                const nome = aluno.nomeDoAluno;
+                const teste = nome.toLowerCase().includes(inputname.toLowerCase());
 
 
+                if (teste) {
+                    const options = document.createElement("a");
+                    
+                    options.style.width = "100%";
+                    options.style.fontSize = "60%"
+                    
+                    options.innerText = `${aluno.nomeDoAluno} || ${aluno.turma} || ${aluno.serie}`;
+                    
+                    modalOptions.appendChild(options);
+                    options.addEventListener("click", () => {
+                        inputNomeAluno.value = aluno.nomeDoAluno;
+                        document.getElementById('inputTurma').value = aluno.turma;
+                        document.getElementById('inputSerie').value = aluno.serie;
+                        modalOptions.style.display = "none";
+                    });
+                }
+            });
+            divNome.appendChild(modalOptions);
+            modalOptions.style.display = "flex";
+        });
+    });
+});
 
-
-
-
-
-
-
+document.addEventListener("click", (event) => {
+    if (!event.target.matches("#inputEmprestimoNomeAluno") && !event.target.matches("option")) {
+        modalOptions.style.display = "none";
+    }
+});
 
 
 // function atualizarDatas() {
@@ -76,13 +128,16 @@ inputNomeAluno.addEventListener("input", (event)=>{
 
 // const btnEnviar =document.getElementById("btnConfirmeEmprestimo")
 
-// btnEnviar.onclick = ()=>{
-//     if(
-//         document.getElementById("inputEmprestimoLivro").value != "" && document.getElementById("inputEmprestimoNomeAluno").value != ""){
-//             alert(document.getElementById("inputEmprestimoLivro").value)
-//             alert(document.getElementById("inputEmprestimoNomeAluno").value)
-//     }
-//     else{
-//         alert("Preencha todos os campos")
-//     }
-// }
+
+const btnEnviar = document.getElementById("btnConfirmeEmprestimo")
+
+btnEnviar.onclick = ()=>{
+    if(
+        document.getElementById("inputEmprestimoLivro").value != "" && document.getElementById("inputEmprestimoNomeAluno").value != ""){
+            alert(document.getElementById("inputEmprestimoLivro").value)
+            alert(document.getElementById("inputEmprestimoNomeAluno").value)
+    }
+    else{
+        alert("Preencha todos os campos")
+    }
+}
