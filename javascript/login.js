cf825743857ca1f1129371231e15dc2bf1a0314b
Thalyson Alves <thalysonalsilva@gmail.com>
2023-05-08 15:26:14 -0300

    silva

--- javascript/login.js
+++ javascript/login.js
@@ -1,16 +1,18 @@
-const btnEnviar = document.querySelector(".btnConfirmeLogin")
+const btnEnviar = document.getElementById("btnenviarlogin")
 const inptnomeDeUsuarioLogin = document.getElementById("nomeDeUsuarioLogin")
 const inptsenhaDeUsuarioLogin = document.getElementById("senhaDeUsuarioLogin")
 
 function Login(){
     if(inptnomeDeUsuarioLogin.value !== "" && inptsenhaDeUsuarioLogin !== ""){
         if(inptnomeDeUsuarioLogin.value == "luciaHelena" && inptsenhaDeUsuarioLogin.value == "asd"){
-            document.querySelector(".secFormLoginUser").style.display = "none"
+            // document.querySelector(".secFormLoginUser").style.display = "none"
+            location.href = "./bibliotech.html"
         }
         else{
             alert("algo errado")
         }
     }
 }
+// console.log(btnEnviar)
 btnEnviar.addEventListener("click", Login)
 
